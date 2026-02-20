71305c0f8025bf5aa03070c4185041f0e4ad8037
Aşkın Ceyhan <askin.ceyhan@bizsim.com>
2026-02-20 22:52:24 +0300

    Sync PackageVersion.cs with v1.15.1
    Update version constant from 1.11.0 to 1.15.1 and release date,
    add .serena to gitignore.

--- Runtime/Core/PackageVersion.cs
+++ Runtime/Core/PackageVersion.cs
@@ -8,8 +8,8 @@ namespace BizSim.GPlay.Games
     /// </summary>
     internal static class PackageVersion
     {
-        public const string Version = "1.11.0";
-        public const string ReleaseDate = "2026-02-13";
+        public const string Version = "1.15.1";
+        public const string ReleaseDate = "2026-02-19";
         public const string PgsV2SdkVersion = "21.0.0";
     }
 }
